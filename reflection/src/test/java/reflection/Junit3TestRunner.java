2726a3966d45f288b25b70efa30db5c223e2714a
yxxnghwan <younghwan960@gmail.com>
2022-09-16 15:27:46 +0900

    feat: reflection 미션 클리어

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,9 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Junit3Test junit3Test = clazz.getConstructor().newInstance();
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> ReflectionTestUtil.executeMethod(junit3Test, method));
     }
 }
