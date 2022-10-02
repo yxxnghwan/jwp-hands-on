3f8c4f96620cf37ee69c9e436ecef50a35682a0e
yxxnghwan <younghwan960@gmail.com>
2022-10-02 17:43:01 +0900

    feat: di 미션 클리어

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +15,34 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void setFields(final Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .forEach(ConsumerWrapper.accept(field -> {
+                    field.setAccessible(true);
+                    if (field.get(bean) == null) {
+                        final Object fieldBean = getBean(field.getType());
+                        field.set(bean, fieldBean);
+                    }
+                }));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow();
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,12 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
+import org.reflections.Reflections;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +16,41 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        final Reflections reflections = new Reflections(rootPackageName);
+        final Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Repository.class);
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        return new DIContainer(classes);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void setFields(final Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .forEach(ConsumerWrapper.accept(field -> {
+                    field.setAccessible(true);
+                    if (field.get(bean) == null) {
+                        final Object fieldBean = getBean(field.getType());
+                        field.set(bean, fieldBean);
+                    }
+                }));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow();
     }
 }
